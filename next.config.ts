984fdac8d3904f2014b4455547dea5542a15a1dd
donggun-Kim-0 <guny3433@gmail.com>
2026-05-27 16:19:29 +0000

    add api url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: "https://fts-backend-ishb.onrender.com",
+  },
 };
 
 export default nextConfig;
